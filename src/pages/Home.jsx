f1ff73865b29310e407106a66f35423ebfa6c93e
Dan Chui <dchui108@gmail.com>
2023-07-23 22:42:35 +0900

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
+function Home() {
+  return (
+    <div>
+      <PageNav />
+
+      <h1>Worldly</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
+}
+
+export default Home;
