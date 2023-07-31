cb4185db3997756f1f0c3b6fdc09dff444698337
Dan Chui <dchui108@gmail.com>
2023-07-31 13:27:45 +0900

    add useCallback to CitiesContext

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
-import styles from "./City.module.css";
 import { useEffect } from "react";
 import { useCities } from "../contexts/CitiesContext";
+import styles from "./City.module.css";
 import Spinner from "./Spinner";
 import BackButton from "./BackButton";
 
@@ -21,7 +21,7 @@ function City() {
     function () {
       getCity(id);
     },
-    [id]
+    [id, getCity]
   );
 
   const { cityName, emoji, date, notes } = currentCity;
